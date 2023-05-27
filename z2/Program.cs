291b0a5c8b47c34d7250bd6bfaa7144b5d9427ab
Maximuszzs <makskartasov04@gmail.com>
2023-05-27 11:44:02 +0300

    z2

---
+++ z2/Program.cs
@@ -0,0 +1,30 @@
+﻿//напишите программу, которая выводит третью цифру заданного числа или сообщает что третьей цифры нет
+
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+
+int fnumber(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
+
+if (check(number) != true)
+System.Console.WriteLine("ретьей цифры нет");
+else
+System.Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");
